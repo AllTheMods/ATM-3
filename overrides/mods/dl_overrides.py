7f7fe4934e57ff58d7eb2334f176f08ce86e3909
martmists <martmists@gmail.com>
2018-05-29 12:52:48 +0200

    General code improvements performance-wise
    Other improvements can be made by using `requests` rather than `urllib`, and using `re.search` instead of `dict(map(...))`, however, I tried to not add any imports.

--- overrides/mods/dl_overrides.py
+++ overrides/mods/dl_overrides.py
@@ -15,20 +15,14 @@ import os, errno, shutil
 from urllib import request, parse
 
 
-#name of file with list of URLs do download
+# Name of file with list of URLs do download
 override_list = "override_mods.txt"
 
 
 def file_lines_list(file):
     """ Reads a plaintext file, retuns a list of all non-blank whitespace-trimmed lines"""
-    lines = []
     with open(file) as file_object:
-        for i, line in enumerate(file_object):
-            the_line = line.strip()
-            if the_line:
-                lines.append(the_line)
-    return lines
-
+        return [line.strip() for line in file_object.readlines() if line.strip()]
 
 def remove_file(filename):
     """
@@ -37,40 +31,42 @@ def remove_file(filename):
 	"""
     try:
         os.remove(filename)
-    except OSError as e: # this would be "except OSError, e:" before Python 2.6
-        if e.errno != errno.ENOENT: # errno.ENOENT = no such file or directory
-            raise # re-raise exception if a different error occurred
+    except OSError as e:  # This would be "except OSError, e:" before Python 2.6
+        if e.errno != errno.ENOENT:  # errno.ENOENT = no such file or directory
+            raise  # Re-raise exception if a different error occurred
 
+def get_filename(url, open_url):
+	info = open_url.info()
+	if 'Content-Disposition' in info:
+		# If the response has Content-Disposition, try to get filename from it
+		
+		# NOTE: For this code, re.search would likely be faster.
+		content_disp = dict(map((lambda x: x.split("=") 
+								 if "=" in x 
+								 else (x, '')), 
+								info['Content-Disposition'].split(';'))}
+		if 'filename' in content_disp:
+			header_filename = content_disp['filename'].strip("\"'")
+			if header_filename:
+				return header_filename
+	# If no filename was found above, parse it out of the final URL.
+	return os.path.basename(urllib.parse.urlsplit(open_url.url)[2])
 
 def download(url, filename=None):
     """ 
     Downloads a file from url without needing to specify destination filename
     CREDIT: https://stackoverflow.com/questions/862173/how-to-download-a-file-using-python-in-a-smarter-way 
     """
-    def get_filename(url, open_url):
-        if 'Content-Disposition' in open_url.info():
-            # If the response has Content-Disposition, try to get filename from it
-            content_disp = dict(map(
-                lambda x: x.strip().split('=') if '=' in x else (x.strip(),''),
-                open_url.info()['Content-Disposition'].split(';')))
-            if 'filename' in content_disp:
-                header_filename = content_disp['filename'].strip("\"'")
-                if header_filename: return header_filename
-        # if no filename was found above, parse it out of the final URL.
-        return os.path.basename(urllib.parse.urlsplit(open_url.url)[2])
-
     r = urllib.request.urlopen(url)
     try:
-        filename = filename or get_filename(url,r)
+        filename = filename or get_filename(url, r)
         remove_file(filename)
         with open(filename, 'wb') as f:
-            shutil.copyfileobj(r,f)
+            shutil.copyfileobj(r, f)
     finally:
         r.close()
 
 
-#read file into list of urls and download each one
+# Read file into list of urls and download each one
 for url in file_lines_list(override_list):
     download(url)
-
-#
